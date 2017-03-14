eb396bce247935e7303eed4d16c8d7017f4190f4
Gerardo Santillana <gerardo.santillana@legrand.us>
2017-03-14 01:02:47 -0400

    Add connect and disconnect even handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);  // websocket server,  listening events,   comunicate server-client
 
 app.use(express.static(publicPath));   //with USE you register a middleware
 
+//register an even listener
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('User was Disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
