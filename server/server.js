3b669f0c29e715d9cc7322a4684b1e839df33226
Gerardo Santillana <gerardo.santillana@legrand.us>
2017-03-14 15:22:12 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -23,15 +23,37 @@ io.on('connection', (socket) => {
   //   createAt: 124123
   // });
 
-  socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
+//socket emit from Admin text  welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date().getTime()
+  });
+
+  //socket broadcast emit from Admin text New user joined
+   socket.broadcast.emit('newMessage', {
+     from: 'Admin',
+     text: 'New user joined',
+    createAt: new Date().getTime()
+    });
+
+   socket.on('createMessage', (message) => {
+     console.log('createMessage', message);
     //emit an event to every connection
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createAt: new Date().getTime()
     });
+
+  //broadcast.emit send message to eveybody but the user specified
+  //  socket.broadcast.emit('newMessage', {
+  //    from: message.from,
+  //    text: message.text,
+  //   createAt: new Date().getTime()
+  //   });
   });
+
 
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
